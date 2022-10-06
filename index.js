dd1789fd764b6b6f759a0dfaf7ad7c713d7cc8b3
Brendan <BP7714.bp@gmail.com>
2022-10-06 04:49:21 +0100

    Logs repeat bounties too

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
 var log = console.log.bind(console);
 
-const myFunc = async () => {
-    let bountiesOnVendor = [],
-        VENDORHASH = 672118013,
-        platformLanguage = window.navigator.language.split('-')[0];
+async function queryVendorBounties(vendorHash, stackUniqueLabel) {
+    
+    let bountiesOnVendor = 0,
+        VENDORHASH = vendorHash; // Set to your chosen vendor hash
 
-    let manifest = await axios.get(`https://www.bungie.net/Platform/Destiny2/Manifest/`),
-        itemDefinitionSuffix = manifest.data.Response.jsonWorldComponentContentPaths[platformLanguage].DestinyInventoryItemDefinition,
-        vendorDefinitionSuffix = manifest.data.Response.jsonWorldComponentContentPaths[platformLanguage].DestinyVendorDefinition;
+    // Fetch manifest
+    const manifest = await axios.get(`https://www.bungie.net/Platform/Destiny2/Manifest/`);
 
-    let definitionsForItems = await axios.get(`https://www.bungie.net/${itemDefinitionSuffix}`, {header: 'Access-Control-Allow-Origin: *'}),
-        inventoryItemDefinitions = definitionsForItems.data;
+    // Get suffix for the item and vendor definitions
+    const suffixForItemDefinitions = manifest.data.Response.jsonWorldComponentContentPaths.en.DestinyInventoryItemDefinition,
+          suffixForVendorDefinitions = manifest.data.Response.jsonWorldComponentContentPaths.en.DestinyVendorDefinition;
 
-    let definitionsForVendors = await axios.get(`https://www.bungie.net/${vendorDefinitionSuffix}`, {header: 'Access-Control-Allow-Origin: *'}),
-        vendor = definitionsForVendors.data[VENDORHASH];
+    const definitionsForItems = await axios.get(`https://www.bungie.net/${suffixForItemDefinitions}`, { header: 'Access-Control-Allow-Origin: *' }), 
+          inventoryItemDefinitions = definitionsForItems.data;
 
-    for (let item of vendor.itemList) {
-        
-        let itemDefinition = inventoryItemDefinitions[item.itemHash];
-        if (itemDefinition.itemType===26) {
-            log(`Item Hash: ${item.itemHash}`, itemDefinition.displayProperties);
+    const definitionsForVendors = await axios.get(`https://www.bungie.net/${suffixForVendorDefinitions}`, { header: 'Access-Control-Allow-Origin: *' }), 
+          vendorData = definitionsForVendors.data[VENDORHASH];
+
+    // Dev
+    // vendorData.itemList.forEach(v => {
+    //     if (inventoryItemDefinitions[v.itemHash].itemType === 26) {
+    //             log(`.`);
+    //             log(`Item Hash: ${v.itemHash},`);
+    //             log(inventoryItemDefinitions[v.itemHash]);
+    //             log(`https://data.destinysets.com/i/InventoryItem/InventoryItem:${v.itemHash}#`);
+    //             log(`.`);
+    //             bountiesOnVendor++;
+    //     };
+    // });
+
+    // Iterate over items in the itemList of the specified vendor
+    for (let v of Object.keys(inventoryItemDefinitions)) {
+
+        if (inventoryItemDefinitions[v].itemType === 26) {
+
+            if (inventoryItemDefinitions[v].inventory.stackUniqueLabel.includes(`${stackUniqueLabel}`)) {
+                log(`.`);
+                log(`Item Hash: ${v},`, inventoryItemDefinitions[v].inventory.stackUniqueLabel);
+                log(inventoryItemDefinitions[v]);
+                log(`https://data.destinysets.com/i/InventoryItem/InventoryItem:${v}#`);
+                log(`.`);
+                bountiesOnVendor++;
+            };
         };
     };
+
+    log(`Amount of Bounties: ${bountiesOnVendor}`);
 };
 
-myFunc();
+// param1 = vendor hash, param2 = substring in common from stackUniqueLabel
+queryVendorBounties(3347378076, 'gambit');
